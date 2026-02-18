ffcb55d342536ae852df12d2cc0d452648eb0a09
MF <tadelebeemnet4@gmail.com>
2026-02-18 22:09:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+  document.getElementById("nav").classList.toggle("show");
+}
+
+function scrollToSection(){
+  document.getElementById("projects").scrollIntoView({behavior:"smooth"});
+}
